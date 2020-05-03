10c5f195242fc57fc725eaa3ad7816a78f4619bb
Katie Rosas <54516136+rosasck@users.noreply.github.com>
2020-05-03 14:07:15 -0700

    fizzbuzz

--- assign3/1-fizzbuzz.js
+++ assign3/1-fizzbuzz.js
@@ -1,1 +1,25 @@
 // Enter your code here
+/*function fizzbuzz(){*/
+let num=1;
+while( num <= 100)
+{
+    if(( num%3 == 0 ) && (num%5 ==0))
+    {
+        document.write('fizzbuzz\n');
+    }
+    else if( (num % 3) == 0 )
+    {
+       document.write('fizz\n');
+    }
+    else if((num % 5) ==0)
+    {
+        document.write('buzz\n');        
+    }
+    else{
+        document.write(num+ '\n');
+    }
+
+    num+=1;
+}
+/*}*/
+
