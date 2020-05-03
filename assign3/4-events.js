cb4867b63d71b1283ab286538d4c35768e05e002
Katie Rosas <54516136+rosasck@users.noreply.github.com>
2020-05-03 16:48:10 -0700

    events done

--- assign3/4-events.js
+++ assign3/4-events.js
@@ -1,1 +1,8 @@
 // Enter your code here
+function checkWindow(){
+var width= window.innerWidth;
+var height=window.innerHeight;
+
+ document.getElementById("wi").innerHTML= width;
+ document.getElementById("he").innerHTML= height;
+}
