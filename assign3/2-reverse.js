82d8668c7858b70d293a8cbc83df8f5ed33dd2d7
Katie Rosas <54516136+rosasck@users.noreply.github.com>
2020-05-03 16:06:31 -0700

    web dev #3
    reverse

--- assign3/2-reverse.js
+++ assign3/2-reverse.js
@@ -1,1 +1,16 @@
 // Enter your code here
+//prompt user for 8 digit number, validate that u have gotten 8 numbers
+//reverses digits
+//print output in alert box
+
+function reverse_fun(){
+let arr2=[];
+let arr1= parseInt(document.getElementById("input").value);
+arr2=rev(arr1);
+alert("input: "+ arr1 +"\n"+ "output: " + arr2 + "\n");
+}
+
+function rev(arr){
+    arr= arr + "";
+    return arr.split("").reverse().join("");
+}
