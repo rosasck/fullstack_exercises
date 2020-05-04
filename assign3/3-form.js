0afc9daf7a20d75a4e79fcc1bfcdf7f9b846ac28
Katie Rosas <54516136+rosasck@users.noreply.github.com>
2020-05-03 17:59:06 -0700

    form done

--- assign3/3-form.js
+++ assign3/3-form.js
@@ -1,1 +1,21 @@
 // Enter your code here
+
+function formCheck()
+{
+    let name= document.getElementById("name").value;
+    let email= document.getElementById("email").value;
+    let msg= document.getElementById("comments").value;
+    let newsletter= document.getElementById("signup").checked;
+
+    console.log("name: "+ name);
+    console.log("email: " + email);
+    console.log("comments: "+ msg);
+    if(newsletter== true)
+    {
+        console.log("newsletter: on");
+    }
+    else
+    {
+        console.log("newsletter: off");
+    }
+}
