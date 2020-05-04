f9e9af4d2bc6444ae549f08134b0af6f4d5c6241
Katie Rosas <54516136+rosasck@users.noreply.github.com>
2020-05-04 14:24:39 -0700

    fail attempt. waiting for lecturee

--- assign3/5-api.js
+++ assign3/5-api.js
@@ -1,7 +1,14 @@
 const url = 'https://restcountries.eu/rest/v2/all';
 
 function displayCountries(){
+     let namer;
+     let population;
+     var response= new XMLHttpRequest();
+     response.open("GET", url, false);
+     response.send(null);
+     namer= response.map((obj) => (obj.name));
 
-    document.getElementById("results").insertAdjacentHTML("beforebegin",'fizzbuzz\n');
+     document.getElementById("results").insertAdjacentHTML("beforebegin",namer);
+     
 
 }
