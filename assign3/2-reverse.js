0265cf52022272153c0437eb7b87e34f7a532873
Katie Rosas <54516136+rosasck@users.noreply.github.com>
2020-05-05 19:39:36 -0700

    i hate api

--- assign3/2-reverse.js
+++ assign3/2-reverse.js
@@ -6,8 +6,13 @@
 function reverse_fun(){
 let arr2=[];
 let arr1= parseInt(document.getElementById("input").value);
+if(arr1<99999999 && arr1>9999999)  {
 arr2=rev(arr1);
 alert("input: "+ arr1 +"\n"+ "output: " + arr2 + "\n");
+}
+else{
+    alert("8 digits required");
+}
 }
 
 function rev(arr){
--- assign3/5-api.js
+++ assign3/5-api.js
@@ -1,23 +1,25 @@
 const url = 'https://restcountries.eu/rest/v2/all';
 
-function displayCountries(){
-     let population;
-     var response= new XMLHttpRequest();
-     response.open("GET", url, true);
-     response.responseType='json';
-     response.send(null);
 
-     response.onload = () => {
-          if(response.status === 200)
+
+function displayCountries(){
+     let resp= new XMLHttpRequest();
+     resp.open('GET', url, true);
+     resp.responseType='json';
+     resp.send();
+
+     resp.onload = () => {
+          if(resp.status === 200)
           {
-               console.log(response.response);
+               var obj= JSON.parse(resp);
+               console.log(obj[name] + ' - ' + obj[population]);
           }
           else
           {
                console.log('Status Code:  ${response.status} - ${response.statusText} ');
           }
      }
-     //document.getElementById("results").insertAdjacentHTML("beforebegin",namer);
      
 
 }
+
