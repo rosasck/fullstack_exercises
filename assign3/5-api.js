b1ce20b772de1dd4cf91785b59624ef30a9781af
Katie Rosas <54516136+rosasck@users.noreply.github.com>
2020-05-05 18:32:07 -0700

    Update 5-api.js

--- assign3/5-api.js
+++ assign3/5-api.js
@@ -3,11 +3,21 @@ const url = 'https://restcountries.eu/rest/v2/all';
 function displayCountries(){
      let population;
      var response= new XMLHttpRequest();
-     response.open("GET", url, false);
+     response.open("GET", url, true);
+     response.responseType='json';
      response.send(null);
-     let namer= response.map(({name}) => ({name}));
 
-     document.getElementById("results").insertAdjacentHTML("beforebegin",namer);
+     response.onload = () => {
+          if(response.status === 200)
+          {
+               console.log(response.response);
+          }
+          else
+          {
+               console.log('Status Code:  ${response.status} - ${response.statusText} ');
+          }
+     }
+     //document.getElementById("results").insertAdjacentHTML("beforebegin",namer);
      
 
 }
