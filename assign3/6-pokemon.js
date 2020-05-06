4bd18553af3b4b4760e64d271ca2ff7d2021bd38
Katie Rosas <54516136+rosasck@users.noreply.github.com>
2020-05-05 22:27:01 -0700

    cant figure out how to move to the next link

--- assign3/6-pokemon.js
+++ assign3/6-pokemon.js
@@ -1,13 +1,17 @@
 const url = 'https://pokeapi.co/api/v2/pokemon/';
 
-let getDataPromise= (url) => {
+let getDataPromise = (url) => {
 fetch(url)
-.then(response =>{
-    console.log('Request Successful', response);
-    return response.json();
-})
-.then(data =>{
-    
+.then(response => response.json())
+.then((data ) =>{
+    console.log("Data", data);
+
+   if(typeof(data)== 'string'){data = JSON.parse(data)}
+    var count=30;
+     data.results.forEach((item) => {
+        document.getElementById("results").insertAdjacentHTML("beforebegin", item.name + ' '); 
+
+      });
 })
 .catch(error =>{
     console.log('Request Failed', error)
