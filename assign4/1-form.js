c9fd9a2f7b18ac388a7a181a5d4b25432be1f633
Katie Rosas <54516136+rosasck@users.noreply.github.com>
2020-05-13 11:53:23 -0700

    ex1 form added

--- assign4/1-form.js
+++ assign4/1-form.js
@@ -1,1 +1,21 @@
-// Enter your code here
+const express= require('express');
+const parser = require('body-parser');
+const app= express();
+const port = 5000;
+
+app.use(
+    parser.urlencoded({
+    extended: false,
+    limit: 1024
+    })
+);
+app.post('/submit', (req, res)=> {
+    console.log("name: " + req.body.name);
+    console.log("email: " + req.body.email);
+    console.log("comments: " + req.body.comments);
+    res.end();
+});
+
+app.listen(port, () =>{
+    console.log('Server running at: http://localhost:5000');
+});
